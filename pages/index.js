fa81f4f8eac7ebe1f0a0badbdc4631c475ec2fa5
hotmailbelike <mdabdullahomar97@gmail.com>
2020-10-30 20:06:26 +0600

    resource picker integration

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
-import { Heading, Page } from "@shopify/polaris";
+import React, { useState } from "react";
+import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const index = () => {
+  const [open, setOpen] = useState(false);
 
-export default Index;
+  const handleSelection = (resources) => {
+    setOpen(false);
+    console.log("handleSelection -> resources", resources);
+  };
+
+  return (
+    <Page
+      title="Product Selector"
+      primaryAction={{
+        content: "Select Products",
+        onAction: () => setOpen(true),
+      }}
+    >
+      <ResourcePicker
+        resourceType="Product"
+        open={open}
+        onCancel={() => setOpen(false)}
+        onSelection={(resources) => handleSelection(resources)}
+      ></ResourcePicker>
+    </Page>
+  );
+};
+
+export default index;
